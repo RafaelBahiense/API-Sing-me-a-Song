d87414b6d66f50db1f49998541fa8955d4d2d0e4
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-07-22 02:40:10 -0300

    Updated app

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express, { Application } from "express";
 import cors from "cors";
 
-import { MainRouter } from "./routes/mainRouter";
+import MainRouter from "./routes/mainRouter";
 import errorHandler from "./utilities/errorHandler";
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/", MainRouter);
+
 app.use(errorHandler);
 
 export default app;
