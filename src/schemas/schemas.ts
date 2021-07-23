aec789f5a0f0fa84068834cd3b28cc8e855b77ad
Rafael Bahiense <rafaelbahiense@id.uff.br>
2021-07-23 15:43:20 -0300

    schema typo

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -6,7 +6,7 @@ const ytRegExp =
 export const RecommendationSchema = Joi.object({
   name: Joi.string().min(1).required(),
   youtubeLink: Joi.string().regex(ytRegExp).required(),
-  genresId: Joi.array().items(Joi.number()).min(1).required(),
+  genresIds: Joi.array().items(Joi.number()).min(1).required(),
 });
 
 export const GenreSchema = Joi.string().min(1).required();
